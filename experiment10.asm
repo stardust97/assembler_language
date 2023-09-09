2d18a719afdadc4a1db227aed34fcb7d2cf5e257
stardust <641765169@qq.com>
2023-09-09 16:30:43 +0800

    fixed bug at experiment 10:

--- experiment10.asm
+++ experiment10.asm
@@ -1,37 +1,33 @@
-assume cs:codesg ds:datasg
+assume cs:codesg ds:datasg ss:stack
 datasg segment
-    'welcome to masm!'
+db      'welcome to masm!'
+db      00000010b,00100100b,00010111b
 datasg ends
 
-codesg segment
+stack segment
+db 8 dup(0)
+stack ends
 
+codesg segment
 start:  mov ax,0B8h
         mov es,ax
+        mov cx,3;3种颜色
+				mov si,0;颜色的索引
+				mov di,0;显存偏移地址
+s:   		mov bx,0;字母的索引
+        push cx
+        mov cx,16;16个字符
 
-        ;绿色
-        mov cx,16
-copy1:  mov ax,0;
-        mov es:[ax],[ax];
-        mov es:2[ax], 00000010b
-        add ax,4
-        loop copy1
+	s1:   mov al,ds:[bx];字符
+        mov ah,ds:16[si];颜色
+        mov es:[di],ax
+        inc bx
+        add di,2
+	loop s1
 
-        ;绿底红色
-        mov cx,16
-copy2:  mov ax,0;
-        mov es:[ax],[ax];
-        mov es:2[ax], 00100100b
-        add ax,4
-        loop copy2
-
-        ;绿底红色
-        mov cx,16
-copy2:  mov ax,0;
-        mov es:[ax],[ax];
-        mov es:2[ax], 00010111b
-        add ax,4
-        loop copy2
-
+			pop cx
+			inc si ;改变颜色
+			loop s
 
 codesg ends
 
